08f068a440f25bdd059aa2f3fa3d364daa8c6b13
unknown <footehud@gmail.com>
2017-07-24 22:31:27 -0400

    adding initial files to the repo

---
+++ liri.js
@@ -0,0 +1,33 @@
+//write the code you need to grab the data from keys.js. Then store the keys in a variable.
+
+//Make it so liri.js can take in one of the following commands:
+
+// node liri.js my-tweets
+//This will show your last 20 tweets and when they were created at in your terminal/bash window.
+
+
+
+// spotify-this-song
+//node liri.js spotify-this-song '<song name here>'
+
+
+
+
+
+// movie-this
+//node liri.js movie-this '<movie name here>'
+//* Title of the movie.
+//* Year the movie came out.
+// * IMDB Rating of the movie.
+// * Rotten Tomatoes Rating of the movie.
+// * Country where the movie was produced.
+// * Language of the movie.
+// * Plot of the movie.
+//* Actors in the movie.
+
+
+
+
+
+// do-what-it-says
+//node liri.js do-what-it-says
